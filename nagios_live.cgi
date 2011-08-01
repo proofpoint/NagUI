387506dc3efc8db35cecfd3f6c1270bef5d2ad94
Isaac Finnegan <ifinnegan@proofpoint.com>
2011-08-01 16:29:14 -0700

    initial commit

---
+++ nagios_live.cgi
@@ -0,0 +1,322 @@
+#!/usr/bin/perl
+# /* Copyright 2010-2011 Proofpoint, Inc. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# */
+
+
+use IO::Socket;
+use lib '.';
+use JSON;
+use CGI;
+use Monitoring::Livestatus;
+
+# JSON version abstraction
+sub eat_json {
+   my ($json_text, $opthash) = @_;
+    return ($JSON::VERSION > 2.0 ? from_json($json_text, $opthash) : JSON->new()->jsonToObj($json_text, $opthash));
+}
+sub make_json {
+   my ($obj, $opthash) = @_;
+    return ($JSON::VERSION > 2.0 ? to_json($obj, $opthash) : JSON->new()->objToJson($obj, $opthash));
+}
+## end JSON functions
+
+# Load up config
+open(CFG, '/opt/pptools/etc/nagui.conf') ||
+open(CFG, '/etc/nagui.conf') ||
+open(CFG, "$ENV{HOME}" . '/nagui.conf') ||
+open(CFG, 'nagui.conf') ||
+die "Cannot open configuration file $!\n";
+my(@conf) = <CFG>;
+close CFG;
+my $config=eat_json(join("\n",@conf)) || die "Error parsing config file $!\n";
+
+my $q=new CGI;
+my $DEBUG=$config->{'debug'};
+
+my $query=$q->param('query');
+my $user=$ENV{REMOTE_USER};
+my @valid_tables=qw( hosts services hostgroups servicegroups contactgroups servicesbygroup servicesbyhostgroup
+	hostsbygroup contacts commands timeperiods downtimes comments log status columns );;
+
+my $output;
+print $q->header('application/json');
+
+$nagios_servers=$config->{'nagios_servers'};
+my $peer;
+
+# only query specified server otherwise query all servers
+if($q->param('peer_name'))
+{
+	$peer=$nagios_servers->{$q->param('peer_name')};
+}
+else
+{
+	$peer=[];
+	foreach(keys(%$nagios_servers))
+	{
+		push(@$peer,{name=>$_,peer=>$nagios_servers->{$_}});
+	}
+}
+
+my $ml=Monitoring::Livestatus->new(
+	#name=>'multisite',
+	verbose=>0,
+	query_timeout=>4,
+	peer=>$peer,
+	errors_are_fatal=>0,
+	warnings=>0
+	);
+
+
+if($query=~/COMMAND/)
+{
+	$query=~s/USERNAME/$user/sg;
+	print STDERR $query if $DEBUG;
+	$query=~s/\|/\n/sg;
+	
+	my $res=$ml->selectall_arrayref($query,{ Slice => {}, Deepcopy => 1, Addpeer=>1  });
+	print &make_json($res,{allow_nonref=>1});
+}
+
+#special nagios status query.
+if($q->param('query') eq 'GET status')
+{
+	my $status_res=[];
+	foreach(keys(%$nagios_servers))
+	{
+		my $st=Monitoring::Livestatus->new(
+			name=> $_,			
+			verbose=>0,
+			query_timeout=>4,
+			peer=>$nagios_servers->{$_},
+			warnings=>0
+			);
+			
+		my $res=$st->selectall_arrayref('GET status',{
+			Slice => {}, 
+			Sum => 1, 
+			Deepcopy => 1,
+			Addpeer=>1
+		});
+		$res->[0]->{'peer_name'}=$_;
+		my $inst_res=$res->[0];
+#		push(@$status_res,$inst_res);
+#		next;
+		# get host counts
+		$res=$st->selectall_arrayref('GET hosts
+Stats: state != 999
+StatsGroupBy: active_checks_enabled',{
+			Slice => {}, 
+			Sum => 1, 
+			Deepcopy => 1,
+			Addpeer => 1
+		});
+		foreach(@$res)
+		{
+			$inst_res->{'hosts_active'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '1');
+			$inst_res->{'hosts_passive'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '0');
+		}
+		# get service counts
+		$res=$st->selectall_arrayref('GET services
+Stats: state != 999
+StatsGroupBy: active_checks_enabled',{
+			Slice => {}, 
+			Sum => 1, 
+			Deepcopy => 1,
+			Addpeer => 1
+		});
+		foreach(@$res)
+		{
+			$inst_res->{'services_active'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '1');
+			$inst_res->{'services_passive'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '0');		
+		}
+		
+		push(@$status_res,$inst_res);
+	}
+	print &make_json($status_res);
+	exit;
+}
+if($q->param('nagiosstatus'))
+{
+	my $hosts_q='GET hosts|Stats: state != 999|StatsGroupBy: active_checks_enabled';
+	my $services_q='GET services|Stats: state != 999|StatsGroupBy: active_checks_enabled';
+	$hosts_q=~s/\|/\n/sg;
+	$services_q=~s/\|/\n/sg;
+	my $host_res=$ml->selectall_arrayref($hosts_q,{ Slice => {},Sum=>1 , Deepcopy => 1, Addpeer=>1 });
+	my $services_res=$ml->selectall_arrayref($services_q,{ Slice => {},Sum=>1 , Deepcopy => 1, Addpeer=>1 });
+	my $status={
+		hosts_active=>0,
+		hosts_passive=>0,
+		services_active=>0,
+		services_passive=>0
+	};
+	foreach(@$host_res)
+	{
+		$status->{'hosts_active'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '1');
+		$status->{'hosts_passive'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '0');
+	}
+	foreach(@$services_res)
+	{
+		$status->{'services_active'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '1');
+		$status->{'services_passive'} = $_->{'state != 999'} if($_->{'active_checks_enabled'} eq '0');		
+	}
+	
+	print &make_json([$status]);
+	exit;
+}
+if($query=~/^GET/)
+{
+	$query=~/^GET\s(\w+)/;
+	my $table=$1;
+	unless(grep(/^$table$/,@valid_tables))
+	{
+		print STDERR "INVALID QUERY: |$table| $query\n" if $DEBUG;
+		exit;
+	}
+	
+	$query=~s/USERNAME/$user/sg;
+
+	if($query)
+	{
+		print STDERR $query if $DEBUG;
+		$query=~s/\|/\n/sg;
+		my $options={ Slice => {}, Sum => 1, Deepcopy => 1, Addpeer=>1  };
+		if($q->param('sum'))
+		{
+			$options->{sum} = 1;
+		}
+		if($query=~/GET\ hostgroups/)
+		{
+			$options->{sum} = 1;
+		}
+		my $res=$ml->selectall_arrayref($query, $options);
+		if($q->param('groupby'))
+		{
+			my $new_res={};
+			
+			foreach my $rec (@$res)
+			{
+				foreach my $key (keys%$rec)
+				{
+					if($new_res->{ $rec->{$q->param('groupby')} } && $new_res->{ $rec->{$q->param('groupby')} }->{$key})
+					{
+						if($key eq $q->param('groupby'))
+						{
+							$new_res->{ $rec->{$q->param('groupby')} }->{$key} = $rec->{$key};
+						}
+						elsif($rec->{$key}=~/^\d+$/)
+						{
+							$new_res->{ $rec->{$q->param('groupby')} }->{$key} = $new_res->{ $rec->{$q->param('groupby')} }->{$key} + ( $rec->{$key} * 1 );
+						}
+						else
+						{
+							$new_res->{ $rec->{$q->param('groupby')} }->{$key} .= " " . $rec->{$key}; 
+						}
+
+					}
+					else
+					{
+						$new_res->{  $rec->{$q->param('groupby')}  }->{$key} = $rec->{$key};
+					}
+				}
+			}
+			$res=[];
+			foreach(keys(%$new_res))
+			{
+				push(@$res,$new_res->{$_});
+			}
+		}
+#TODO normalize all these loops into one
+		if($q->param('mode') eq 'treeloader' && $q->param('nodetext'))
+		{
+			foreach (@$res)
+			{
+				$_->{'text'}=$_->{$q->param('nodetext')};
+				$_->{'leaf'}=JSON::true if $q->param('leaf');
+				$_->{'checked'}=0;
+#				$_->{'loader'}=$q->param('loader') if $q->param('loader');
+			}
+		}
+		if($q->param('peer_name'))
+		{
+			foreach (@$res)
+			{
+					$_->{'peer_name'}=$q->param('peer_name');
+			}
+		}
+		foreach (@$res)
+		{
+			$_->{'peer_name'}= $_->{'peer_name'} || $_->{'peer_name'};
+			if($q->param('nodetext') eq 'hostsvc')
+			{
+				$_->{'text'}=$_->{'host_name'} . ' ' . $_->{'description'};					
+			}
+		}
+		if($q->param('status'))
+		{
+			foreach (@$res)
+			{
+				if($q->param('nodetext') eq 'hostsvc')
+				{
+					$_->{'text'}=$_->{'host_name'} . ' ' . $_->{'description'};					
+				}
+				else
+				{
+					$_->{'text'}=defined $_->{$q->param('nodetext')} ? $_->{$q->param('nodetext')} : $q->param('nodetext');
+				}
+				if($q->param('status') eq 'statecount' )
+				{				
+					$_->{'status'}='OK: ' . $_->{'state = 0'} . ' Warn: '. $_->{'state = 1'} . ' Crit: ' . $_->{'state = 2'} . ' Unknown: ' .  $_->{'state = 3'} ;
+					$_->{'num_services_ok'}=$_->{'state = 0'};
+					$_->{'num_services_warn'}=$_->{'state = 1'};
+					$_->{'num_services_crit'}=$_->{'state = 2'};
+					$_->{'num_services_unknown'}=$_->{'state = 3'};
+				}
+				elsif($q->param('status') eq 'services' )
+				{				
+					$_->{'status'}='Hosts: ' . $_->{'num_hosts'} . ' ' if $_->{'num_hosts'};
+					$_->{'status'}.='OK: ' . $_->{'num_services_ok'} . ' Warn: '. $_->{'num_services_warn'} . ' Crit: ' . $_->{'num_services_crit'} . ' Unknown: ' .  $_->{'num_services_unknown'} ;
+					$_->{'status'}='';
+				}
+				elsif($q->param('status') eq 'service_count' )
+				{				
+					$_->{'status'}= $_->{'alias'} . ': ' . $_->{'num_services'};
+				}
+				elsif($q->param('status') eq 'problems')
+				{
+#					$_->{'text'}= $_->{'acknowledged'} == '0' ? 'Unhandled Problems' : 'Acknowledged';
+					$_->{'status'}= 'Total: ' . $_->{'state > 0'} ;
+					print STDERR "DEBUG" . make_json($_) if $DEBUG;
+				}
+				else
+				{
+					$_->{'status'}=$_->{'plugin_output'};					
+				}
+			}
+			# if( scalar(@$res) == 0 )
+			# {
+			# 	push(@$res,{text=> $q->param('nodetext') , status=> 'Total: 0'});
+			# }			
+		}
+		else
+		{
+			$_->{'status'}=$_->{'plugin_output'};					
+		}
+	
+		print &make_json($res);
+		exit;
+	}
+
+}
---
+++ nagiosnew.js
@@ -0,0 +1,498 @@
+/* Copyright 2010-2011 Proofpoint, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+Ext.define('NagUI.FilterItem',{
+	extend: 'Ext.menu.CheckItem',
+	alias: 'widget.filteritem',
+	hideOnClick:true,
+	checked: false,
+	handler:function(b,e){
+		var tab=Ext.getCmp('nagios_views').getActiveTab();
+		if(b.checked)
+		{
+			tab.store.customHostFilters.push(b.HostFilter);
+			tab.store.customServiceFilters.push(b.ServiceFilter);
+		}
+		else
+		{
+			Ext.Array.remove(tab.store.customHostFilters,b.HostFilter);
+			Ext.Array.remove(tab.store.customServiceFilters,b.ServiceFilter);
+		}
+	}
+	
+});
+
+
+NagUI.nagiosServerStatusWindow=function(config){
+	var t=new Ext.TabPanel();
+	var w=new Ext.Window({
+		height:600,
+		title:'Nagios Servers',
+		width: 500,
+		layout:'fit',
+		items: t
+	});
+	w.show();
+	w.getEl().mask('Loading...');
+	Ext.Ajax.request({
+		url: NagUI.url,
+		method: 'GET',
+		params: {
+			query:'GET services|Filter: active_checks_enabled = 1|Stats: min latency|Stats: max latency|Stats: avg latency|Stats: max execution_time|Stats: avg execution_time|Stats: min execution_time'
+		},
+		window:w,
+		success: function(r,o){
+			var stats=Ext.decode(r.responseText);
+			Ext.each(stats,function(i){
+				Ext.apply(NagUI.nagiosServers.getById(i.peer_name).data,i);
+			});
+			t.removeAll(true);
+			NagUI.nagiosServers.each(function(i){
+				NagUI.log(i.data);
+				t.add(
+					{
+						xtype: 'panel',
+						layout: 'border',
+						title: i.get('peer_name'),
+						items:[
+							Ext.create('Ext.grid.property.Grid',{
+								region: 'center',
+								source:i.data,
+								nameColumnWidth:220,
+								tbar:[
+								{
+									text: 'Actions',
+									menu:{
+										items:[
+											{
+												text:'Enable Notifications',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'ENABLE_NOTIFICATIONS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Disable Notifications',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'DISABLE_NOTIFICATIONS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Start Executing Host Checks',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'START_EXECUTING_HOST_CHECKS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Stop Executing Host Checks',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'STOP_EXECUTING_HOST_CHECKS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Start Executing Service Checks',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'START_EXECUTING_SVC_CHECKS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Stop Executing Service Checks',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'STOP_EXECUTING_SVC_CHECKS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Start Accepting Passive Service Checks',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'START_ACCEPTING_PASSIVE_SVC_CHECKS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Stop Accepting Passive Service Checks',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'STOP_ACCEPTING_PASSIVE_SVC_CHECKS9';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text:'Enable Event Handlers',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'ENABLE_EVENT_HANDLERS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Disable Event Handlers',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'DISABLE_EVENT_HANDLERS';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Enable Performance Data Processing',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'ENABLE_PERFORMANCE_DATA';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Disable Performance Data Processing',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'DISABLE_PERFORMANCE_DATA';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											},
+											{
+												text: 'Send Restart Cmd',
+												handler:function(){
+													var str='COMMAND ['+ new Date().format('U') + '] ' + 'RESTART_PROGRAM';
+													sendNagiosCommand(str,'GET',t.getActiveTab().title);
+												}
+											}
+										]
+									}									
+								}
+								]
+							})
+						]
+					}
+				);		
+			});	
+			o.window.getEl().unmask();
+			t.getActiveTab().doLayout();		
+		}
+	});
+	return w;
+}
+
+
+function nagiosStatus()
+{
+	NagUI.nagiosServers.load({callback:function(){
+		var data={
+			hosts_active:0,
+			hosts_passive:0,
+			services_active:0,
+			services_passive:0
+		};
+		NagUI.nagiosServers.each(function(r){
+			data.hosts_active=data.hosts_active+ (r.get('hosts_active') ? r.get('hosts_active') : 0);
+			data.hosts_passive=data.hosts_passive+(r.get('hosts_passive') ? r.get('hosts_passive') : 0);
+			data.services_active=data.services_active+(r.get('services_active') ? r.get('services_active') : 0 );
+			data.services_passive=data.services_passive+(r.get('services_passive') ? r.get('services_passive') : 0 );
+		});
+		var str='Hosts (active/passive): ' + data.hosts_active + " / " + data.hosts_passive;
+		str+='<br/>Services (active/passive): ' + data.services_active + ' / ' + data.services_passive;
+		Ext.getCmp('statusbox').getEl().update(str);			
+		//Ext.getCmp('statusboxbutton').setText(str);			
+	}});	
+}
+
+
+
+function getColumnsForNagiosType(nagiosType)
+{
+	if(nagiosType[nagiosType.length-1] == 's')
+	{
+		nagiosType=nagiosType.slice(0,nagiosType.length-1);
+	}
+	switch(nagiosType)
+	{
+		case 'hostgroup' : 
+			return 'name num_hosts num_services_ok num_services_warn num_services_crit num_services_unknown';
+			break;			
+		case 'host' : 
+			return 'notification_period address groups notes notes_url scheduled_downtime_depth next_check downtimes_with_info notifications_enabled comments_with_info acknowledged last_check last_hard_state_change name num_services_ok num_services_warn num_services_crit num_services_unknown state';
+			break;
+		case 'service' :	
+			return 'notes host_notes notification_period host_address notes_url scheduled_downtime_depth host_scheduled_downtime_depth next_check downtimes_with_info notifications_enabled comments_with_info acknowledged last_check last_hard_state_change plugin_output host_name description host_groups groups state';
+			break;
+	}
+	
+}
+
+NagUI.setupLogWindow=function(c,target)
+{
+	NagUI.log(c);
+	var l=new NagUI.NagiosLogGrid();
+	var w=new Ext.Window({
+		height: 500,
+		width: 800,
+		title: 'Logs for ' + (c['host_name'] ? c['host_name'] : '') +  ' ' + ( c['service_description'] ? c['service_description'] : ''),
+		layout: 'fit',
+		items:l
+	});
+	w.show(target,function(){
+		l.getLogs(c);
+	});
+	return l;
+}
+
+
+function nagiosSearch(term,opts)
+{
+	if(!term || typeof term == 'object')
+	{
+		term=Ext.getCmp('search_input').getValue();
+	}
+	var searchPanel=Ext.getCmp('nagios_search');
+	Ext.getCmp('nagios_views').setActiveTab(searchPanel);
+	var searchView=searchPanel.view;
+	var search=searchPanel.store.getRootNode();
+	var type=Ext.getCmp('search_type').searchtype;
+	search.collapse();
+	Ext.fly(searchView.getNode(search)).addCls(searchView.loadingCls);
+	
+	search.data.parms={
+		mode: 'treeloader',
+		nodetext: ( type=='service' ? 'hostsvc' : 'name' ) ,
+		query: 'search'
+	};
+	if(type == 'hostgroup')
+	{
+		NagUI.nodeQueries.search= 'GET ' + type + 's|Columns: ' + getColumnsForNagiosType(type) + '|Filter: name ~~ ' + term;		
+	}
+	if(type == 'host')
+	{
+		NagUI.nodeQueries.search= 'GET ' + type + 's|Columns: ' + getColumnsForNagiosType(type) + '|Filter: name ~~ ' + term + '|Filter: address ~~ ' + term + '|Filter: alias ~~ ' + term + '|Filter: notes ~~ ' + term + '|Or: 4';		
+	}
+	if(type == 'service')
+	{
+		NagUI.nodeQueries.search= 'GET ' + type + 's|Columns: ' + getColumnsForNagiosType(type) + '|Filter: host_name ~~ ' + term + '|Filter: host_address ~~ ' + term + '|Filter: host_alias ~~ ' + term +'|Filter: description ~~ ' + term + '|Filter: plugin_output ~~ ' + term + '|Filter: host_notes ~~ ' + term + '|Or: 6';				
+	}
+	search.expand();
+	
+}
+function toggleAutoRefresh(i,tree)
+{
+	var menu=i.ownerCt;
+	if(!i.checked)
+	{
+		tree.autoRefresh=0;
+		if(typeof tree.refreshTask!='undefined')
+		{
+			Ext.TaskManager.stop(tree.refreshTask);
+		}
+		delete tree.refreshTask;
+	}
+	else
+	{
+		if(typeof tree.refreshTask!='undefined')
+		{
+			Ext.TaskManager.stop(tree.refreshTask);
+		}
+		tree.autoRefresh=i.time;		
+		tree.refreshTask={
+			run: function(){
+				tree.refresh();
+			},
+			interval: i.time * 1000
+		};
+		Ext.TaskManager.start(tree.refreshTask);	
+		menu.items.each(function(k){
+			if(typeof k.setChecked == 'undefined')
+			{
+				return;
+			}
+			if(k.time == i.time)
+			{
+				return;
+			}
+			else
+			{
+				k.setChecked(false);
+			}
+		});			
+	}
+}
+
+
+function nagiosTextRender(value,o,r,row,col,store){
+	if(typeof r.state=='undefined')
+	{
+		return value;
+	}
+	else
+	{
+		
+	}
+}
+function nagiosBoolRender(value)
+{
+	if(value == '0' || value == 0)
+	{
+		return 'Off';
+	}
+	return 'On';
+}
+function nagiosStateRender(value)
+{
+	if(value == '0') return '<span class=statusOK>OK</span>';
+	if(value == '1') return '<span class=statusWarning>Warning</span>';
+	if(value == '2') return '<span class=statusCritical>Critical</span>';
+	if(value == '3') return '<span class=statusUnknown>Unknown</span>';
+}
+
+function setNagiosInfo(node,target)
+{
+	//Ext.getCmp('nagiosdetail').getEl().update('');
+	target.setLoading(true);
+	var filter_keys={
+		hostgroup:'Filter: name',
+		host:'name',
+		service:''
+	};
+	var query='GET ' + node.data.nagios_type + 's';
+	query+= node.data.nagios_type == 'host' ? '|Filter: name = ' + node.data.name : '';
+	query+= node.data.nagios_type == 'service' ? '|Filter: description ~ ' + node.data.description : '';
+	query+= node.data.nagios_type == 'service' ? '|Filter: host_name = ' + node.data.host_name : '';
+	query+= node.data.nagios_type == 'hostgroup' ? '|Filter: name = ' + node.data.name : '';
+
+	Ext.Ajax.request({
+		url: NagUI.url,
+		method: 'GET',
+		nagios_type: node.data.nagios_type,
+		params:{
+			query:query,
+			mode: 'treeloader',
+			method:'GET',
+			nodetext:'name',
+			status: node.data.nagios_type + 's',
+			peer_name: ( node.data.nagios_type == 'host' || node.data.nagios_type == 'service' ? node.data.peer_name : '' )
+		},
+		success: function(r,o)
+		{
+			if(r.responseText)
+			{
+				var d=Ext.decode(r.responseText);
+				var info=nagiosTemplates[o.nagios_type].apply(d[0]);
+				target.update(replaceURLWithHTMLLinks(info));
+				var detail=Ext.getCmp('nagiosdetaildata');
+				detail.setSource(d[0]);
+				// detail.invalidateScroller();
+				
+			}	
+			target.setLoading(false);
+		},
+		failure: function(r,o)
+		{
+			target.setLoading(false);
+		}
+	});
+}
+
+
+function addCustomView(name){
+	var views=Ext.getCmp('nagios_views');
+	if(typeof name !='string')
+	{
+		 name='View ' + views.items.getCount();
+	}
+	var newcustomview= views.add(new Ext.tree.NagiosTree({
+		title: name,
+		stateId: name.replace(' ','-'),
+		allowDrop:true,
+		rootVisible:true,
+		closable:true,
+		listeners:{
+			'itemclick': updateNagiosInfoPanel,
+		},
+		// stateful:true,
+		// statefulNodes:true,
+		viewConfig: {
+			animate: false,
+			allowCopy: true,
+            plugins: {
+                ptype: 'treeviewdragdrop',
+				allowContainerDrops: true,
+				ddGroup: 'nagiosDD',
+                enableDrop: true,
+				onContainerDrop:function(source, e, data){
+					data.copy=true;
+					var node=dropTree.store.root.appendChild(data.node);
+					if(node)
+					{
+						node.data.allowDrop=false;
+					}
+					return true;
+				},
+			    onContainerOver : function(dd, e, data) {
+					data.copy=true;
+			        return e.getTarget('.' + this.indicatorCls) ? this.currentCls : this.dropAllowed;
+			    }
+			},
+			listeners: {
+				beforedrop : function(de,data) {
+					data.copy=true;
+				}
+			}
+
+        },
+		store:new Ext.data.NagiosStore({
+			root: {
+				loaded:true,
+				expanded:true,
+				text: 'Custom View',
+				meta: 1
+			},
+			customHostFilters:['Filter: name != dummy-host-all-services'],
+			customServiceFilters:['Filter: host_name !~ dummy-host-all-services']
+		})
+	}));
+	views.setActiveTab(newcustomview);
+	newcustomview.store.getRootNode().loaded=true;
+	NagUI.log(newcustomview);
+	return newcustomview;
+}
+
+function replaceURLWithHTMLLinks(text) {
+  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;\*]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  return text.replace(exp,"<a target=_new href='$1'>$1</a>"); 
+}
+function updateNagiosInfoPanel(view,node)
+{
+	var nagiosdetail=Ext.getCmp('nagiosdetail');
+	setNagiosInfo(node,nagiosdetail);
+	nagiosdetail.node=node;
+}
+
+Ext.notify = function(){
+    var msgCt;
+
+    function createBox(t, s){
+       return '<div class="msg"><h3>' + t + '</h3><p>' + s + '</p></div>';
+    }
+    return {
+        msg : function(title, format){
+            if(!msgCt){
+                msgCt = Ext.core.DomHelper.insertFirst(document.body, {id:'msg-div'}, true);
+            }
+            var s = Ext.String.format.apply(String, Array.prototype.slice.call(arguments, 1));
+            var m = Ext.core.DomHelper.append(msgCt, createBox(title, s), true);
+            m.hide();
+            m.slideIn('t').ghost("t", { delay: 1000, remove: true});
+        },
+
+        init : function(){
+        }
+    };
+}();
+
