d1f7b641b49f00b44b29980f97dbaa5f93be51b7
Isaac Finnegan <ifinnegan@proofpoint.com>
2011-08-15 13:00:13 -0700

    adding inline doc

---
+++ custom.js
@@ -0,0 +1,10 @@
+/*
+This file is automatically loaded after the nagui JS files.  
+Apply any install specific customizations here
+*/
+
+//customNodes are added to the Nagios Browser pane (left-hand) 
+//before the Hostgroups and Services 
+
+
+NagUI.customNodes=[];
