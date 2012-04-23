cf9350c67a25d04e815f446140e233349897ac4b
Isaac Finnegan <isaacfinnegan@gmail.com>
2012-04-23 01:32:00 -0700

    slight refactor to help debug

--- nagiosnew.js
+++ nagiosnew.js
@@ -553,25 +553,20 @@ function restoreCustomViews()
 function saveCustomView(view)
 {
 	var savedViews=Ext.getCmp('saved_views');
+	var newChild={
+		text:view.getStateId(), 
+		id:view.getStateId(),
+		saveid:view.getStateId(),
+		viewstate: view.getState(),
+		leaf: true 
+	};
 	if( savedViews.getRootNode().childNodes[1].findChild('text',view.getStateId()) )
 	{
-		savedViews.getRootNode().childNodes[1].replaceChild({
-			text:view.getStateId(), 
-			id:view.getStateId(),
-			saveid:view.getStateId(),
-			viewstate: view.getState(),
-			leaf: true 
-		},savedViews.getRootNode().childNodes[1].findChild('text',view.getStateId()));
+		savedViews.getRootNode().childNodes[1].replaceChild(newChild ,savedViews.getRootNode().childNodes[1].findChild('text',view.getStateId()));
 	}
 	else
 	{
-		savedViews.getRootNode().childNodes[1].appendChild({
-			text:view.getStateId(), 
-			id:view.getStateId(),
-			saveid:view.getStateId(),
-			viewstate: view.getState(),
-			leaf:true 
-		});
+		savedViews.getRootNode().childNodes[1].appendChild(newChild);
 	}
 }
 
